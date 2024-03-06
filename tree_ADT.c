6521a4b37aa91d9f4e6c3883b87a3a941b600de1
HS <64366425+Harshil042001@users.noreply.github.com>
2024-03-06 11:36:41 +0530

    Update tree_ADT.c

--- tree_ADT.c
+++ tree_ADT.c
@@ -1,3 +1,13 @@
+/*  Group No : 7
+2020B1A71605P Samarth Trivedi
+2020B1A71612P Harshil Sethi
+2020B1A70644P Nitish Sahani
+2020B3A70786P Neel Dedhia
+2020B3A70794P Harsh Rathi
+2020B5A70604P Divyan Poddar
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "tree_ADT.h"
